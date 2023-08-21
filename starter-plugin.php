49de2ae67ce107bcba8abc182005eeccb4497142
khaledmv <khaledhossionmv@gmail.com>
2023-08-21 09:27:00 +0600

    Initialize starter  boilerplate of wordpress pluign

---
+++ starter-plugin.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * @package  StarterPlugin
+ */
+/*
+Plugin Name: Starter Plugin
+Plugin URI: http://khaleddd.me/plugin
+Description: WordPress Starter plugin.
+Version: 1.0.0
+Author: Khaled Hossain
+Author URI: http://khaleddd.me
+License: GPLv2 or later
+Text Domain: starter-plugin
+*/
+
+/*
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+Copyright 2005-2015 Automattic, Inc.
+*/
+
+defined( 'ABSPATH' ) or die( 'Hey, what are you doing here? You silly human!' );
+
+if ( file_exists( dirname( __FILE__ ) . '/vendor/autoload.php' ) ) {
+	require_once dirname( __FILE__ ) . '/vendor/autoload.php';
+}
+
+define( 'PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
+define( 'PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+
+if ( class_exists( 'Inc\\Init' ) ) {
+	Inc\Init::register_services();
+}
